f8c4937a976700ca9aa07d7161fca692697473d6
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-17 11:38:46 -0500

    6.17.26 11.38am Finished Lucas project 1.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // 1. Change "cute.dog"to your name (Keep the quotation marks!)
-let username = "cute.dog";
+let username = "MiriJones";
 
 // 2. Change the number of followers from 10000 to another number.
-let followers = 10000;
+let followers = 3;
 
+let localm = "Newburgh, IN";
 
 // DO NOT CHANGE THE CODE BELOW
 const names = document.querySelectorAll(".name");
 names.forEach((name) => name.textContent = username);
 document.querySelector(".followers").innerHTML = followers;
+document.querySelector(".localm").innerHTML = localm;
--- variables.js
+++ variables.js
@@ -1,13 +1,14 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+let favoriteColor = "Blue";
+console.log(favoriteColor);
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood = "Southern style quinoa";
+console.log(favoriteFood);
 
 // 2. Create a variable called `favoriteNumber`.
-
-
-
+let favoriteNumber = 13;
+console.log(favoriteNumber);
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
